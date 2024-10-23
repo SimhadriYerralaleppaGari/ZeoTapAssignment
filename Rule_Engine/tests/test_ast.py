17173be4145e3ad8bf206019b4142fd269922c7b
coderskom <coders.kom@gmail.com>
2024-10-23 17:04:33 +0530

    Final commit

--- Rule_Engine/tests/test_ast.py
+++
@@ -1,9 +0,0 @@
-# tests/test_ast.py
-import unittest
-from rule_parser import create_rule
-
-class TestAST(unittest.TestCase):
-    def test_create_rule(self):
-        rule = "age > 30 AND department = 'Sales'"
-        ast = create_rule(rule)
-        self.assertIsNotNone(ast)  # Ensure AST is created
--- Rule_Engine/tests/test_combiner.py
+++
@@ -1,9 +0,0 @@
-# tests/test_combiner.py
-import unittest
-from rule_combiner import combine_rules
-
-class TestCombiner(unittest.TestCase):
-    def test_combine_rules(self):
-        rules = ["age > 30", "salary > 50000"]
-        combined_ast = combine_rules(rules)
-        self.assertIsNotNone(combined_ast)  # Ensure AST is combined
